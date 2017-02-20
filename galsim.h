902753e0db98a8176a78352a30017922af62ac0c
annshchekina <annshchekina@gmail.com>
2017-02-20 14:51:20 +0100

    header needed for the test

---
+++ galsim.h
@@ -0,0 +1,5 @@
+void calculation(star* stars, star* stars_buffer, int N_stars, double delta_t);
+
+void keep_within_box(float* xA, float* yA);
+
+int galsim(int N, char * filename, int steps, double delta_t, int graphics);
